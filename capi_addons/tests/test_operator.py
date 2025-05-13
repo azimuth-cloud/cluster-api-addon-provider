5fbf3a0dc9d1722a1dc1a85a8a7219fe3f2f36fb
John Garbutt <john.garbutt@stackhpc.com>
2025-05-13 15:58:41 +0100

    Add initial unit test

--- capi_addons/tests/test_operator.py
+++ capi_addons/tests/test_operator.py
@@ -1,10 +1,27 @@
 import unittest
+from unittest import mock
 
 from capi_addons import operator
 
 
-class TestOperator(unittest.TestCase):
-    def test_handle_secret_event(self):
-        operator.handle_secret_event(
+class TestOperator(unittest.IsolatedAsyncioTestCase):
+    @mock.patch.object(operator, "handle_config_event")
+    @mock.patch.object(operator, "create_ek_client")
+    async def test_handle_secret_event(
+        self, mock_create_ek_client, mock_handle_config_event
+    ):
+        operator.ek_config = mock.AsyncMock()
+
+        await operator.handle_secret_event(
             type="type", name="name", namespace="namespace", body={"key": "value"}
         )
+
+        mock_handle_config_event.assert_awaited_once_with(
+            mock.ANY,
+            "type",
+            "name",
+            "namespace",
+            {"key": "value"},
+            "secret.addons.stackhpc.com",
+        )
+        mock_create_ek_client.assert_called_once_with()
