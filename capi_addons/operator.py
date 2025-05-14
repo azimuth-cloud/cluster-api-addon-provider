32fa4c406b56dc99394b64d290e6986b3bb52f94
John Garbutt <john.garbutt@stackhpc.com>
2025-05-14 13:21:22 +0100

    Fix up tox (#271)

--- capi_addons/operator.py
+++ capi_addons/operator.py
@@ -7,37 +7,32 @@ import logging
 import sys
 import tempfile
 
-import kopf
-
 from easykube import Configuration, ApiError, resources as k8s, PRESENT
-
+import kopf
 from kube_custom_resource import CustomResourceRegistry
-
+from pydantic.json import pydantic_encoder
 from pyhelm3 import Client as HelmClient
 from pyhelm3 import errors as helm_errors
 
-from . import models, template, utils
-from .models import v1alpha1 as api
-from .config import settings
+from capi_addons import models, template, utils
+from capi_addons.models import v1alpha1 as api
+from capi_addons.config import settings
 
 
 logger = logging.getLogger(__name__)
 
-
 # Initialise the template loader
 template_loader = template.Loader()
 
-
 # Initialise an easykube config object from the environment
-from pydantic.json import pydantic_encoder
-
-ek_config = Configuration.from_environment(json_encoder=pydantic_encoder)
+ek_config: Configuration
 
 
 def create_ek_client():
     """
     Returns an easykube client for the default config.
     """
+    global ek_config
     return ek_config.async_client(default_field_manager=settings.easykube_field_manager)
 
 
@@ -51,6 +46,8 @@ async def apply_settings(**kwargs):
     """
     Apply kopf settings and register CRDs.
     """
+    global ek_config
+    ek_config = Configuration.from_environment(json_encoder=pydantic_encoder)
     kopf_settings = kwargs["settings"]
     kopf_settings.persistence.finalizer = f"{settings.annotation_prefix}/finalizer"
     kopf_settings.persistence.progress_storage = kopf.AnnotationsProgressStorage(
