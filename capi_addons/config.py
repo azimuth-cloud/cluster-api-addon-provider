b072697827545b5fe0420c813b972f0961a32b74
John Garbutt <john.garbutt@stackhpc.com>
2025-06-26 10:34:33 +0100

    Add tox tests (#270)
    Co-authored-by: bertiethorpe <84867280+bertiethorpe@users.noreply.github.com>
    Co-authored-by: bertiethorpe <bertie443@gmail.com>

--- capi_addons/config.py
+++ capi_addons/config.py
@@ -1,113 +1,124 @@
-import typing as t
-
-from pydantic import Field, conint, constr, field_validator, ValidationInfo
-
-from configomatic import Configuration as BaseConfiguration, Section, LoggingConfiguration
+from configomatic import (
+    Configuration as BaseConfiguration,
+)
+from configomatic import (
+    LoggingConfiguration,
+    Section,
+)
+from pydantic import Field, ValidationInfo, conint, constr, field_validator
 
 
 class HelmClientConfiguration(Section):
     """
     Configuration for the Helm client.
     """
+
     #: The default timeout to use with Helm releases
     #: Can be an integer number of seconds or a duration string like 5m, 5h
-    default_timeout: t.Union[int, constr(min_length = 1)] = "1h"
+    default_timeout: int | constr(min_length=1) = "1h"
     #: The executable to use
     #: By default, we assume Helm is on the PATH
-    executable: constr(min_length = 1) = "helm"
+    executable: constr(min_length=1) = "helm"
     #: The maximum number of revisions to retain in the history of releases
     history_max_revisions: int = 10
     #: Indicates whether to verify TLS when pulling charts
     insecure_skip_tls_verify: bool = False
     #: The directory to use for unpacking charts
     #: By default, the system temporary directory is used
-    unpack_directory: t.Optional[str] = None
+    unpack_directory: str | None = None
 
 
 class Configuration(
     BaseConfiguration,
-    default_path = "/etc/capi-addon-provider/config.yaml",
-    path_env_var = "CAPI_ADDON_PROVIDER_CONFIG",
-    env_prefix = "CAPI_ADDON_PROVIDER"
+    default_path="/etc/capi-addon-provider/config.yaml",
+    path_env_var="CAPI_ADDON_PROVIDER_CONFIG",
+    env_prefix="CAPI_ADDON_PROVIDER",
 ):
     """
     Top-level configuration model.
     """
+
     #: The logging configuration
-    logging: LoggingConfiguration = Field(default_factory = LoggingConfiguration)
+    logging: LoggingConfiguration = Field(default_factory=LoggingConfiguration)
 
     #: The API group of the cluster CRDs
-    api_group: constr(min_length = 1) = "addons.stackhpc.com"
+    api_group: constr(min_length=1) = "addons.stackhpc.com"
     #: The prefix to use for operator annotations
-    annotation_prefix: constr(min_length = 1) = Field(None, validate_default = True)
+    annotation_prefix: constr(min_length=1) = Field(None, validate_default=True)
     #: A list of categories to place CRDs into
-    crd_categories: t.List[constr(min_length = 1)] = Field(
-        default_factory = lambda: ["cluster-api", "capi-addons"]
+    crd_categories: list[constr(min_length=1)] = Field(
+        default_factory=lambda: ["cluster-api", "capi-addons"]
     )
 
     #: The field manager name to use for server-side apply
-    easykube_field_manager: constr(min_length = 1) = "cluster-api-addon-provider"
+    easykube_field_manager: constr(min_length=1) = "cluster-api-addon-provider"
 
     #: The amount of time (seconds) before a watch is forcefully restarted
-    watch_timeout: conint(gt = 0) = 600
+    watch_timeout: conint(gt=0) = 600
 
     #: The delay to use for temporary errors by default
-    temporary_error_delay: conint(gt = 0) = 15
+    temporary_error_delay: conint(gt=0) = 15
 
     #: The Helm client configuration
-    helm_client: HelmClientConfiguration = Field(default_factory = HelmClientConfiguration)
+    helm_client: HelmClientConfiguration = Field(
+        default_factory=HelmClientConfiguration
+    )
 
     #: Label indicating that an addon belongs to a cluster
-    cluster_label: constr(min_length = 1) = Field(None, validate_default = True)
+    cluster_label: constr(min_length=1) = Field(None, validate_default=True)
     #: Label indicating the target namespace for the addon
-    release_namespace_label: constr(min_length = 1) = Field(None, validate_default = True)
+    release_namespace_label: constr(min_length=1) = Field(None, validate_default=True)
     #: Label indicating the name of the release for the addon
-    release_name_label: constr(min_length = 1) = Field(None, validate_default = True)
+    release_name_label: constr(min_length=1) = Field(None, validate_default=True)
     #: Label indicating that a configmap or secret should be watched for changes
-    watch_label: constr(min_length = 1) = Field(None, validate_default = True)
+    watch_label: constr(min_length=1) = Field(None, validate_default=True)
     #: Prefix to use for annotations containing a configmap checksum
-    configmap_annotation_prefix: constr(min_length = 1) = Field(None, validate_default = True)
+    configmap_annotation_prefix: constr(min_length=1) = Field(
+        None, validate_default=True
+    )
     #: Prefix to use for annotations containing a secret checksum
-    secret_annotation_prefix: constr(min_length = 1) = Field(None, validate_default = True)
+    secret_annotation_prefix: constr(min_length=1) = Field(None, validate_default=True)
     #: Annotation to use to trigger restarts of workloads in lifecycle hooks
-    lifecycle_hook_restart_annotation: constr(min_length = 1) = Field(None, validate_default = True)
+    lifecycle_hook_restart_annotation: constr(min_length=1) = Field(
+        None, validate_default=True
+    )
 
-    @field_validator("annotation_prefix", mode = "before")
+    @field_validator("annotation_prefix", mode="before")
     @classmethod
     def default_annotation_prefix(cls, v, info: ValidationInfo):
-        return v or info.data['api_group']
+        return v or info.data["api_group"]
 
-    @field_validator("cluster_label", mode = "before")
+    @field_validator("cluster_label", mode="before")
     @classmethod
     def default_cluster_label(cls, v, info: ValidationInfo):
         return v or f"{info.data['api_group']}/cluster"
 
-    @field_validator("release_namespace_label", mode = "before")
+    @field_validator("release_namespace_label", mode="before")
     @classmethod
     def default_release_namespace_label(cls, v, info: ValidationInfo):
         return v or f"{info.data['api_group']}/release-namespace"
 
-    @field_validator("release_name_label", mode = "before")
+    @field_validator("release_name_label", mode="before")
     @classmethod
     def default_release_name_label(cls, v, info: ValidationInfo):
         return v or f"{info.data['api_group']}/release-name"
 
-    @field_validator("watch_label", mode = "before")
+    @field_validator("watch_label", mode="before")
     @classmethod
     def default_watch_label(cls, v, info: ValidationInfo):
         return v or f"{info.data['api_group']}/watch"
 
-    @field_validator("configmap_annotation_prefix", mode = "before")
+    @field_validator("configmap_annotation_prefix", mode="before")
     @classmethod
     def default_configmap_annotation_prefix(cls, v, info: ValidationInfo):
         return v or f"configmap.{info.data['annotation_prefix']}"
 
-    @field_validator("secret_annotation_prefix", mode = "before")
+    @field_validator("secret_annotation_prefix", mode="before")
     @classmethod
     def default_secret_annotation_prefix(cls, v, info: ValidationInfo):
         return v or f"secret.{info.data['annotation_prefix']}"
 
-    @field_validator("lifecycle_hook_restart_annotation", mode = "before")
+    @field_validator("lifecycle_hook_restart_annotation", mode="before")
     @classmethod
     def default_lifecycle_hook_restart_annotation(cls, v, info: ValidationInfo):
         return v or f"{info.data['annotation_prefix']}/restarted-at"
--- capi_addons/template.py
+++ capi_addons/template.py
@@ -3,10 +3,8 @@ import json
 import typing as t
 
 import jinja2
-
-from pydantic.json import pydantic_encoder
-
 import yaml
+from pydantic.json import pydantic_encoder
 
 from . import utils
 
@@ -15,27 +13,24 @@ class Loader:
     """
     Class for returning objects created by rendering YAML templates from this package.
     """
-    def __init__(self, **globals):
+
+    def __init__(self, **globals):  # noqa: A002
         # We only need to render strings and want to shut templates in a sandbox
-        # So we use the base loader which doesn't have an implementation for including templates
-        self._env = jinja2.Environment(loader = jinja2.BaseLoader(), autoescape = False)
+        # So we use the base loader which doesn't have an implementation for including
+        # templates
+        self._env = jinja2.Environment(loader=jinja2.BaseLoader(), autoescape=False)
         self._env.globals.update(globals)
         self._env.filters.update(
-            mergeconcat = utils.mergeconcat,
-            fromyaml = yaml.safe_load,
+            mergeconcat=utils.mergeconcat,
+            fromyaml=yaml.safe_load,
             # In order to benefit from correct serialisation of a variety of objects,
             # including Pydantic models, generic iterables and generic mappings, we go
-            # via JSON with the Pydantic encoder
-            toyaml = lambda obj: yaml.safe_dump(
-                json.loads(
-                    json.dumps(
-                        obj,
-                        default = pydantic_encoder
-                    )
-                )
+            # via JSON with the Pydantic encoder
+            toyaml=lambda obj: yaml.safe_dump(
+                json.loads(json.dumps(obj, default=pydantic_encoder))
             ),
-            b64encode = lambda data: base64.b64encode(data).decode(),
-            b64decode = lambda data: base64.b64decode(data).decode()
+            b64encode=lambda data: base64.b64encode(data).decode(),
+            b64decode=lambda data: base64.b64decode(data).decode(),
         )
 
     def render_string(self, template_str: str, **params: t.Any) -> str:
@@ -43,18 +38,19 @@ class Loader:
         Render the given template string with the given params and return the result as
         a string.
 
-        By default, this uses the safe environment which does not have access to templates.
+        By default, this uses the safe environment which does not have access to
+        templates.
         """
         return self._env.from_string(template_str).render(**params)
 
-    def yaml_string(self, template_str: str, **params: t.Any) -> t.Dict[str, t.Any]:
+    def yaml_string(self, template_str: str, **params: t.Any) -> dict[str, t.Any]:
         """
         Render the given template string with the given params, parse the result as YAML
         and return the resulting object.
         """
         return yaml.safe_load(self.render_string(template_str, **params))
 
-    def yaml_string_all(self, template_str: str, **params: t.Any) -> t.Dict[str, t.Any]:
+    def yaml_string_all(self, template_str: str, **params: t.Any) -> dict[str, t.Any]:
         """
         Render the given template string with the given params, parse the result as YAML
         and return the resulting objects.
